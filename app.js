1225faec4fbc57f8fd09d654835487fac0eedf0c
Emília Querino <emiliaquerino@gmail.com>
2024-01-14 23:19:05 -0300

    cria as funções selectRating e submit

--- app.js
+++ app.js
@@ -5,3 +5,23 @@ const resultValueDOM = document.querySelector('.card__result-value')
 
 let activeRatingNumber;
 
+function selectRating(number) {
+  const selectedRatingNumberDOM = ratingNumbersDOM[number-1]
+  selectedRatingNumberDOM.classList.add('card__rating-number--active')
+  
+  if(activeRatingNumber) {
+    const activeRatingDOM = ratingNumbersDOM[activeRatingNumber-1]
+    activeRatingDOM.classList.remove('card__rating-number--active')
+  }
+
+  activeRatingNumber = number;
+}
+
+function submit() {
+  if(!activeRatingNumber) return;
+  
+  mainCardDOM.classList.add('hide')
+  thankyouCardDOM.classList.remove('hide')
+
+  resultValueDOM.textContent = activeRatingNumber;
+}
