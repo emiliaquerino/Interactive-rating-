e8537ae88b800a8b84cce575e95094158a4123e7
Emília Querino <emiliaquerino@gmail.com>
2024-01-14 23:17:41 -0300

    cria arquivo js e as variáveis

---
+++ app.js
@@ -0,0 +1,7 @@
+const ratingNumbersDOM = document.querySelectorAll('.card__rating-number')
+const mainCardDOM = document.querySelector('.card--main')
+const thankyouCardDOM = document.querySelector('.card--thank-you')
+const resultValueDOM = document.querySelector('.card__result-value')
+
+let activeRatingNumber;
+
